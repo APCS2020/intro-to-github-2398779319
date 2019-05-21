a68f4569941194e4cd02d4b6f02ea1b6005a6619
刘嘉睿 jerry liu <50470632+2398779319@users.noreply.github.com>
2019-05-22 07:53:03 +0800

    rth
    rah

--- readm2.java
+++ readm2.java
@@ -1,1 +1,19 @@
-
+public static String weekday(int d){
+    if (d==1){
+        return"monday";
+    } else if (d == 2){
+        return"tuesday";
+    } else if (d == 3){
+        return"wednesday";
+    } else if (d == 4){
+        return"thursday";
+    } else if (d == 5){
+        return"friday";
+    } else if (d == 6){
+        return"saturday";
+    } else if (d == 7){
+        return"sunday";
+    } else {
+        return "you have to enter a number between 1-7";
+    }
+}
